88051fc4aca2bd47d24899dc64d48616525f947d
Leslie <xuyan@leslie-invest.com>
2026-07-11 03:51:50 +0800

    Configure Supabase cloud sync

--- portfolio-tracker/supabase-config.js
+++ portfolio-tracker/supabase-config.js
@@ -4,6 +4,6 @@
 // 3. 填到下面两个字段
 // 4. 配置为空时，网站会自动保持“本机私有数据”模式
 window.PORTFOLIO_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://qlunajakvisorlxayjzr.supabase.co",
+  anonKey: "sb_publishable_kUB7lz5W2uOUxDR4VdhMoA_GdLpLYMF"
 };
